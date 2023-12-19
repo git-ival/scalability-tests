308961094021ed6dd0276e4be03bdd9b020681ac
Iramis Valentin <85186645+git-ival@users.noreply.github.com>
2023-12-19 22:01:15 +0000

    set upstream as target cluster

--- bin/run_tests.mjs
+++ bin/run_tests.mjs
@@ -7,7 +7,6 @@ import {
   q,
   runCollectingJSONOutput,
   runCollectingOutput,
-  getAppAddressesFor,
 } from "./lib/common.mjs"
 import { k6_run } from "./lib/k6.mjs"
 
@@ -18,24 +17,12 @@ const USER_COUNT = 1000
 
 // Refresh k6 files on the tester cluster
 const clusters = runCollectingJSONOutput(`terraform -chdir=${terraformDir()} output -json`)["clusters"]["value"]
-const tester = clusters["tester"]
-helm_install("k6-files", dir("charts/k6-files"), tester, "tester", {})
+const upstream = clusters["upstream"]
+helm_install("k6-files", dir("charts/k6-files"), upstream, "tester", {})
 
 // Create config maps
 const commit = runCollectingOutput("git rev-parse --short HEAD").trim()
 const downstreams = Object.entries(clusters).filter(([k, v]) => k.startsWith("downstream"))
-
-const upstream = clusters["upstream"]
-
-// create users and roles
-const upstreamAddresses = getAppAddressesFor(upstream)
-const rancherURL = upstreamAddresses.localNetwork.httpsURL
-const rancherClusterNetworkURL = upstreamAddresses.clusterNetwork.httpsURL
-k6_run(tester,
-  { BASE_URL: rancherClusterNetworkURL, USERNAME: "admin", PASSWORD: ADMIN_PASSWORD, ROLE_COUNT: ROLE_COUNT, USER_COUNT: USER_COUNT },
-  { commit: commit, cluster: "upstream", test: "create_roles_users.mjs", Roles: ROLE_COUNT, Users: USER_COUNT },
-  "k6/create_roles_users.js", true
-)
 
 // Output access details
 console.log("*** ACCESS DETAILS")
@@ -43,7 +30,6 @@ console.log()
 
 console.log(`*** UPSTREAM CLUSTER`)
 console.log(`    Rancher UI: ${rancherURL} (admin/${ADMIN_PASSWORD})`)
-
 console.log(`    Kubernetes API:`)
 console.log(`export KUBECONFIG=${q(upstream["kubeconfig"])}`)
 console.log(`kubectl config use-context ${q(upstream["context"])}`)
@@ -62,12 +48,3 @@ for (const [name, downstream] of downstreams) {
   }
   console.log()
 }
-
-console.log(`*** TESTER CLUSTER`)
-const testerAddresses = getAppAddressesFor(tester)
-const grafanaURL = testerAddresses.localNetwork.httpURL
-console.log(`    Grafana UI: ${grafanaURL}/grafana/d/a1508c35-b2e6-47f4-94ab-fec400d1c243/test-results?orgId=1&refresh=5s&from=now-30m&to=now (admin/${ADMIN_PASSWORD})`)
-for (const [node, command] of Object.entries(tester["node_access_commands"])) {
-  console.log(`    Node ${node}: ${q(command)}`)
-}
-console.log()
