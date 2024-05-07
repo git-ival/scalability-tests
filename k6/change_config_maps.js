2e3f4fe251d5e4e0538b15315faab7ade86666d1
Iramis Valentin <85186645+git-ival@users.noreply.github.com>
2024-05-07 19:28:30 +0000

    update name generation for configmap tests, add thresholds for create_config_maps.js

--- k6/change_config_maps.js
+++ k6/change_config_maps.js
@@ -9,61 +9,63 @@ const data = encoding.b64encode("a".repeat(1))
 const vus = 5
 const duration = '2h'
 const rate = 1
+const nameScheme = __ENV.NAME_SCHEME
 
 // Option setting
 const kubeconfig = k8s.kubeconfig(__ENV.KUBECONFIG, __ENV.CONTEXT)
 const baseUrl = __ENV.BASE_URL
 
 export const options = {
-    insecureSkipTLSVerify: true,
-    tlsAuth: [
-        {
-            cert: kubeconfig["cert"],
-            key: kubeconfig["key"],
-        },
-    ],
-
-    summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(95)', 'p(99)', 'count'],
-
-    scenarios: {
-        create: {
-            executor: 'constant-vus',
-            exec: 'change',
-            vus: vus,
-            duration: duration,
-        },
+  insecureSkipTLSVerify: true,
+  tlsAuth: [
+    {
+      cert: kubeconfig["cert"],
+      key: kubeconfig["key"],
     },
-    thresholds: {
-        checks: ['rate>0.99']
-    }
+  ],
+
+  summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(95)', 'p(99)', 'count'],
+
+  scenarios: {
+    change: {
+      executor: 'constant-vus',
+      exec: 'change',
+      vus: vus,
+      duration: duration,
+    },
+  },
+  thresholds: {
+    checks: ['rate>0.99']
+  }
 };
 
 // Test functions, in order of execution
 
 export function setup() {
-    // delete leftovers, if any
-    k8s.del(`${baseUrl}/api/v1/namespaces/${namespace}`)
+  // delete leftovers, if any
+  k8s.del(`${baseUrl}/api/v1/namespaces/${namespace}`)
 
-    // create empty namespace
-    const body = {
-        "metadata": {
-            "name": namespace,
-        },
-    }
-    k8s.create(`${baseUrl}/api/v1/namespaces`, body)
+  // create empty namespace
+  const body = {
+    "metadata": {
+      "name": namespace,
+    },
+  }
+  k8s.create(`${baseUrl}/api/v1/namespaces`, body)
 }
 
 export function change() {
-    const name = `test-config-map-${exec.scenario.name}-${exec.scenario.iterationInTest}`
-    const body = {
-        "metadata": {
-            "name": name,
-            "namespace": namespace
-        },
-        "data": {"data": data}
-    }
+  const namePrefix = `${nameScheme}` || `test-config-map-${exec.scenario.name}`
+  const name = `${namePrefix}-${exec.scenario.iterationInTest}`.toLowerCase()
+  const body = {
+    "metadata": {
+      "name": name,
+      "namespace": namespace
+    },
+    "data": { "data": data }
+  }
 
-    k8s.create(`${baseUrl}/api/v1/namespaces/${namespace}/configmaps`, body)
-    sleep(1.0/rate)
-    k8s.del(`${baseUrl}/api/v1/namespaces/${namespace}/configmaps/${name}`)
+  k8s.create(`${baseUrl}/api/v1/namespaces/${namespace}/configmaps`, body)
+  sleep(1.0 / rate)
+  k8s.del(`${baseUrl}/api/v1/namespaces/${namespace}/configmaps/${name}`)
 }
